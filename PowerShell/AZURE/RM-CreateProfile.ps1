7040a2ed0adf29d9f1dd784c4dadef73cf0a37f8
jamlab <work@jamik.de>
2018-06-20 22:42:32 +0200

    Initial

---
+++ PowerShell/AZURE/RM-CreateProfile.ps1
@@ -0,0 +1,6 @@
+Login-AzureRmAccount
+Get-AzureRmSubscription
+Select-AzureRmSubscription -SubscriptionName "Visual Studio Enterprise – MPN"
+# Old
+#Save-AzureRmProfile -Path "B:\MSDN-profile.json"
+Save-AzureRmContext -Path "B:\MSDN-profile.json"
